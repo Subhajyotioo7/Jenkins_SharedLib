ab483c9cf074bbd10fd2b03814421bcad6017382
Subhajyoti Mahato <programingsubhajyoti@gmail.com>
2026-08-05 03:06:42 +0530

    Refactor docker_push function parameter order

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,7 @@
-def call(String Project, String ImageTag, String dockerhubuser){
-  withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'dockerhubpass', usernameVariable: 'dockerhubuser')]) {
-      sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
-  }
-  sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
+// vars/docker_push.groovy
+def call(String dockerhubuser, String Project, String ImageTag) {
+    withCredentials([usernamePassword(credentialsId: 'docker', usernameVariable: 'DOCKERHUB_USER', passwordVariable: 'DOCKERHUB_PASS')]) {
+        sh "docker login -u ${DOCKERHUB_USER} -p ${DOCKERHUB_PASS}"
+        sh "docker push ${DOCKERHUB_USER}/${Project}:${ImageTag}"
+    }
 }
