e282bad6ab4631b01cc7d5eb5dec468820adefda
Subhajyoti Mahato <programingsubhajyoti@gmail.com>
2026-08-06 04:45:37 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,7 @@
 // Define function
-def call(String ProjectName, String ImageTag){
-  sh "docker build -t ${ProjectName}:${ImageTag} ."
+//def call(String ProjectName, String ImageTag){
+//  sh "docker build -t ${ProjectName}:${ImageTag} ."
+//}
+def call(String ImageName, String ImageTag, String DockerHubUser){
+    sh "docker build -t ${DockerHubUser}/${ImageName}:${ImageTag} ."
 }
