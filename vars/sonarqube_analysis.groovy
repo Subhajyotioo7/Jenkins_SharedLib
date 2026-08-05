9d298fdf40de220d5fe0de103c0fa1ec398aec42
Subhajyoti Mahato <programingsubhajyoti@gmail.com>
2026-08-06 04:29:53 +0530

    Refactor SonarQube analysis function with parameters

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -1,14 +1,5 @@
-def call(){
-    script {
-        timeout(time: 5, unit: 'MINUTES') {
-            def qg = waitForQualityGate()
-            if (qg.status != 'OK') {
-                echo "⚠️ WARNING: SonarQube Quality Gate failed with status: ${qg.status}"
-                echo "Pipeline will continue anyway (non-blocking mode)."
-                currentBuild.result = 'UNSTABLE'
-            } else {
-                echo "✅ SonarQube Quality Gate passed."
-            }
-        }
-    }
+def call(String SonarQubeAPI, String Projectname, String ProjectKey){
+  withSonarQubeEnv("${SonarQubeAPI}"){
+      sh "$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${Projectname} -Dsonar.projectKey=${ProjectKey} -X"
+  }
 }
